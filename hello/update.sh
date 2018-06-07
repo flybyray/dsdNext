768a77aeaf0cf1f3083d9ca8948abb097b9565b3
Robert Rettig <rrettig@synapplix.de>
2018-06-07 11:03:58 +0200

    fix check

--- hello/update.sh
+++ hello/update.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+change1=1
+change2=1
 if diff index1.html index.html &>/dev/null; then
-  cp index2.html index.html
+  change1=$?
 fi
 if diff index2.html index.html &>/dev/null; then
+  change2=$?
+fi
+if [ $change1 -eq 0 ]; then
+  cp index2.html index.html
+fi
+if [ $change2 -eq 0 ]; then
   cp index1.html index.html
 fi
 docker build -t ndgit/hello:next .
